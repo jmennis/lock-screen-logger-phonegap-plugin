f4b2a07549de61c4aa10928e1316ca4271dd3fcb
John Ennis <john.ennis@fivebyfiveglobal.com>
2016-03-02 16:53:27 +0000

    updated android

--- src/android/Screen.java
+++ src/android/Screen.java
@@ -1,62 +1,5 @@
 public class ScreenReceiver extends BroadcastReceiver {
 
-    // THANKS JASON
-    public static boolean wasScreenOn = true;
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-            // DO WHATEVER YOU NEED TO DO HERE
-            wasScreenOn = false;
-            System.out.println("SCREEN TURNED OFF - Fire javascript");
-        } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-            // AND DO WHATEVER YOU NEED TO DO HERE
-            wasScreenOn = true;
-            System.out.println("SCREEN TURNED ON - Fire javascript");
-        }
-    }
-
-}
-
-public class ExampleActivity extends Activity {
-
-   @Override
-    protected void onCreate() {
-        // INITIALIZE RECEIVER
-        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        BroadcastReceiver mReceiver = new ScreenReceiver();
-        registerReceiver(mReceiver, filter);
-        // YOUR CODE
-    }
-
-    @Override
-    protected void onPause() {
-        // WHEN THE SCREEN IS ABOUT TO TURN OFF
-        if (ScreenReceiver.wasScreenOn) {
-            // THIS IS THE CASE WHEN ONPAUSE() IS CALLED BY THE SYSTEM DUE TO A SCREEN STATE CHANGE
-            System.out.println("SCREEN TURNED OFF");
-        } else {
-            // THIS IS WHEN ONPAUSE() IS CALLED WHEN THE SCREEN STATE HAS NOT CHANGED
-        }
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        // ONLY WHEN SCREEN TURNS ON
-        if (!ScreenReceiver.wasScreenOn) {
-            // THIS IS WHEN ONRESUME() IS CALLED DUE TO A SCREEN STATE CHANGE
-            System.out.println("SCREEN TURNED ON");
-        } else {
-            // THIS IS WHEN ONRESUME() IS CALLED WHEN THE SCREEN STATE HAS NOT CHANGED
-        }
-        super.onResume();
-    }
-}
-
-public class ScreenReceiver extends BroadcastReceiver {
-
     private boolean screenOff;
 
     @Override
@@ -90,8 +33,10 @@ public static class UpdateService extends Service {
             boolean screenOn = intent.getBooleanExtra("screen_state", false);
             if (!screenOn) {
                 // YOUR CODE
+                System.out.println("SCREEN TURNED OFF");
             } else {
                 // YOUR CODE
+                System.out.println("SCREEN TURNED ON");
             }
         }
 }
