95e50312383a76f266e108b6107097f68d385a1f
John Ennis <john.ennis@fivebyfiveglobal.com>
2016-03-02 17:23:57 +0000

    more updates

--- src/android/Screen.java
+++ src/android/Screen.java
@@ -1,45 +1,72 @@
-public class ScreenReceiver extends BroadcastReceiver {
-    System.out.println("PLUGIN: ScreenReceiver");
-    private boolean screenOff;
+package com.jmennis.lockscreenloggerplugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Screen extends CordovaPlugin {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        System.out.println("PLUGIN: onReceive");
-        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-            screenOff = true;
-            System.out.println("SCREEN TURNED OFF");
-        } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-            screenOff = false;
-            System.out.println("SCREEN TURNED ON");
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("init")) {
+            System.out.println("PLUGIN: Screen - init");
+            // String name = data.getString(0);
+            // String message = "Hello, " + name;
+            callbackContext.success(message);
+            return true;
+        } else if (action.equals("getScreenStatus")) {
+            System.out.println("PLUGIN: Screen - getScreenStatus");
+            callbackContext.success(message);
+            return true;
+        } else {
+            return false;
         }
-        Intent i = new Intent(context, UpdateService.class);
-        i.putExtra("screen_state", screenOff);
-        context.startService(i);
     }
-
 }
-
-public static class UpdateService extends Service {
-        @Override
-        public void onCreate() {
-            System.out.println("PLUGIN: UpdateService onCreate");
-            super.onCreate();
-            // REGISTER RECEIVER THAT HANDLES SCREEN ON AND SCREEN OFF LOGIC
-            IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-            filter.addAction(Intent.ACTION_SCREEN_OFF);
-            BroadcastReceiver mReceiver = new ScreenReceiver();
-            registerReceiver(mReceiver, filter);
-        }
-
-        @Override
-        public void onStart(Intent intent, int startId) {
-            boolean screenOn = intent.getBooleanExtra("screen_state", false);
-            if (!screenOn) {
-                // YOUR CODE
-
-            } else {
-                // YOUR CODE
-
-            }
-        }
-}
+//
+// public class ScreenReceiver extends BroadcastReceiver {
+//     System.out.println("PLUGIN: ScreenReceiver");
+//     private boolean screenOff;
+//
+//     @Override
+//     public void onReceive(Context context, Intent intent) {
+//         System.out.println("PLUGIN: onReceive");
+//         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+//             screenOff = true;
+//             System.out.println("SCREEN TURNED OFF");
+//         } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+//             screenOff = false;
+//             System.out.println("SCREEN TURNED ON");
+//         }
+//         Intent i = new Intent(context, UpdateService.class);
+//         i.putExtra("screen_state", screenOff);
+//         context.startService(i);
+//     }
+//
+// }
+//
+// public static class UpdateService extends Service {
+//         @Override
+//         public void onCreate() {
+//             System.out.println("PLUGIN: UpdateService onCreate");
+//             super.onCreate();
+//             // REGISTER RECEIVER THAT HANDLES SCREEN ON AND SCREEN OFF LOGIC
+//             IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+//             filter.addAction(Intent.ACTION_SCREEN_OFF);
+//             BroadcastReceiver mReceiver = new ScreenReceiver();
+//             registerReceiver(mReceiver, filter);
+//         }
+//
+//         @Override
+//         public void onStart(Intent intent, int startId) {
+//             boolean screenOn = intent.getBooleanExtra("screen_state", false);
+//             if (!screenOn) {
+//                 // YOUR CODE
+//
+//             } else {
+//                 // YOUR CODE
+//
+//             }
+//         }
+// }
--- www/Screen.js
+++ www/Screen.js
@@ -1,34 +1,30 @@
-var Screen = {
-               m_screenOn: function () {
+    var Screen = {
+                m_screenOn: function () {},
+                m_screenOff: function () {},
+
+                start: function (screenOnCallback, screenOffCallback, errorCallback) {
+                    this.m_screenOn = screenOnCallback;
+                    this.m_screenOff = screenOffCallback;
+                    !errorCallback && (errorCallback = function () {}
+                );
+
+                cordova.exec(function (arg) {}, errorCallback, "Screen", "init", []);
                },
-               m_screenOff: function () {
-               },
-               
-               
-               start: function (screenOnCallback, screenOffCallback, errorCallback) {
-               this.m_screenOn = screenOnCallback;
-               this.m_screenOff = screenOffCallback;
-               !errorCallback && (errorCallback = function () {
-                                  });
-               
-               cordova.exec(function (arg) {
-                            }, errorCallback, "Screen", "init", []);
-               },
-               
+
                status: function (resultCallback, cancelCallback, errorCallback) {
-               cordova.exec(function (arg) {
+                   cordova.exec(function (arg) {
                             resultCallback(arg);
-                            }, errorCallback, "Screen", "getScreenStatus", []);
+                        }, errorCallback, "Screen", "getScreenStatus", []);
                },
-               
+
                screenon: function (data) {
-               this.m_screenOn(data);
+                   this.m_screenOn(data);
                },
-               
+
                screenoff: function (data) {
-               this.m_screenOff(data);
-               }
+                       this.m_screenOff(data);
+                   }
                };
-               
-               
+
+
                module.exports = Screen;
